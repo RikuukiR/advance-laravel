7e933e3ead8ab06ed2cb250aff09a5c41ced9cca
Riku <rikusugawara.1103@gamil.com>
2024-09-08 19:28:36 +0900

    update

--- src/routes/web.php
+++ src/routes/web.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\SessionController;
 use App\Models\Person;
+use App\Models\Product;
+use App\Http\Controllers\PenController;
 
 Route::get('/', [AuthorController::class, 'index']);
 Route::get('/find', [AuthorController::class, 'find']);
@@ -34,3 +36,19 @@ Route::get('softdelete/get', function() {
     $person = Person::onlyTrashed()->get();
     dd($person);
 });
+
+Route::get('softdelete/absolute', function() {
+    $result = Person::onlyTrashed()->forceDelete();
+    echo $result;
+});
+
+Route::get('uuid',function() {
+    $products = Product::all();
+    foreach($products as $product){
+        echo $product.'<br>';
+    }
+});
+
+Route::get('fill', [PenController::class,'fillPen']);
+Route::get('create', [PenController::class,'createPen']);
+Route::get('insert', [PenController::class,'insertPen']);
