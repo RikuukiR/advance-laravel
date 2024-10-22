4f1120e2a33cc8d284fdb892fd9a7b00d0df771d
Riku <rikusugawara.1103@gamil.com>
2024-10-22 20:26:01 +0900

    add

--- src/app/Http/Controllers/AuthorController.php
+++ src/app/Http/Controllers/AuthorController.php
@@ -8,79 +8,22 @@ use App\Http\Requests\AuthorRequest;
 
 class AuthorController extends Controller
 {
-    public function index()
+    public function add()
     {
-        $authors = Author::all();
-        return view('index', ['authors' => $authors]);
-    }
-    // データ追加用ページの表示
-    public function add(){
         return view('add');
     }
 
-    public function create(AuthorRequest $request){
+    public function create(Request $request)
+    {
         $form = $request->all();
         Author::create($form);
         return redirect('/');
     }
 
-    public function edit(Request $request){
-        $author = Author::find($request->id);
-        return view('edit', ['form' => $author]);
-    }
-
-    public function update(AuthorRequest $request){
-        $form = $request->all();
-        unset($form['_token']);
-        Author::find($request->id)->update($form);
-        return redirect('/');
-    }
-
-    public function delete(Request $request){
-        $author = Author::find($request->id);
-        return view('delete', ['author' => $author]);
-    }
-
-    public function remove(Request $request)
+    public function index()
     {
-        Author::find($request->id)->delete();
-        return redirect('/');
+        $authors = Author::all();
+        return view('index', ['authors' => $authors]);
     }
 
-    public function find()
-    {
-        return view('find', ['input' => '']);
-    }
-
-    public function search(Request $request)
-    {
-        // $item = Author::where('name', 'LIKE',"%{$request->input}%")->first();
-        $item = Author::where('name', $request->input)->first();
-        $param = [
-            'input' => $request->input,
-            'item' => $item
-        ];
-        return view('find', $param);
-    }
-
-    public function bind(Author $author)
-    {
-        $data = [
-            'item'=>$author,
-        ];
-        return view('author.binds', $data);
-    }
-
-    public function verror()
-    {
-    return view('verror');
-    }
-
-    public function relate(Request $request) //追記
-{
-    $hasItems = Author::has('books')->get();
-    $noItems = Author::doesntHave('books')->get();
-    $param = ['hasItems' => $hasItems, 'noItems' => $noItems];
-    return view('author.index',$param);
-}
 }
