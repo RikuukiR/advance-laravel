c310f532ebf81c73c2b9991917eb21c2e84526c4
Riku <rikusugawara.1103@gamil.com>
2025-03-09 18:50:03 +0900

    2-4

--- src/app/Http/Controllers/AuthorController.php
+++ src/app/Http/Controllers/AuthorController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Models\Author;
 use Illuminate\Http\Request;
+use App\Http\Requests\AuthorRequest;
 
 class AuthorController extends Controller
 {
     public function index()  //データ一覧ページの表示
     {
         $authors = Author::all();
+        // dd($authors);
         return view('index', ['authors' => $authors]);
     }
 
@@ -18,9 +20,10 @@ class AuthorController extends Controller
         return view('add');
     }
 
-    public function create(Request $request)  //データ追加機能
+    public function create(AuthorRequest $request)  //データ追加機能
     {
         $form = $request->all();
+        // dd($form);
         Author::create($form);
         return redirect('/');
     }
@@ -31,9 +34,10 @@ class AuthorController extends Controller
         return view('edit', ['form' => $author]);
     }
 
-    public function update(Request $request)  //更新機能
+    public function update(AuthorRequest $request)  //更新機能
     {
         $form = $request->all();
+        // dd($form);
         unset($form['_token']);
         Author::find($request->id)->update($form);
         return redirect('/');
@@ -48,6 +52,30 @@ class AuthorController extends Controller
     public function remove(Request $request)  //削除機能
     {
         Author::find($request->id)->delete();
+        // dd($request->id);
         return redirect('/');
+    }
+
+    public function find()
+    {
+        return view('find', ['input' => '']);
+    }
+
+    public function search(Request $request)
+    {
+        $item = Author::where('name', 'LIKE', "%{$request->input}%")->first();
+        $param = [
+            'input' => $request->input,
+            'item' => $item
+        ];
+        return view('find', $param);
+    }
+
+    public function bind(Author $author)
+    {
+        $data = [
+            'item'=>$author,
+        ];
+        return view('author.binds', $data);
     }
 }
