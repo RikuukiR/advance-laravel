1409cfa9366c915a3d440df801a95bdd6587f2a4
Riku <rikusugawara.1103@gamil.com>
2025-03-14 09:55:19 +0900

    3-7

--- src/app/Models/Author.php
+++ src/app/Models/Author.php
@@ -11,13 +11,22 @@ class Author extends Model
 
     protected $fillable = ['name', 'age', 'nationality'];
 
+    public static $rules = array(
+        'name' => 'required',
+        'age' => 'integer|min:0|max:150',
+        'nationality' => 'required'
+    );
     public function getDetail()
     {
-        $txt = 'ID:' . $this->id . '' . $this->name . '(' . $this->age . '歳' . ')' . $this-> nationality;
+        $txt = 'ID:' . $this->id . ' ' . $this->name . '(' . $this->age .  '才' . ') ' . $this->nationality;
         return $txt;
     }
-
-    public function books(){
+    public function book()
+    {
+        return $this->hasOne('App\Models\Book');
+    }
+    public function books()
+    {
         return $this->hasMany('App\Models\Book');
     }
 }
